580964d5e38564af4937002250b264a96bd627de
Lucas Costa <lucas.costa@lit.ifce.edu.br>
2018-03-27 21:08:45 -0300

    init implemntation of RMI communication

---
+++ src/main/java/MainPackage/DNSRun.java
@@ -0,0 +1,14 @@
+package MainPackage;
+
+
+import java.io.IOException;
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
+public class DNSRun {
+    public static void main(String[] args) throws IOException {
+//        JogoImpl obj = new JogoImpl();
+//        LocateRegistry.createRegistry(1099);
+//        Naming.rebind("rmi://localhost/Jogo",obj);
+    }
+}
