25130a7fe5fe938799a2f35b380b3bc36eeb4cd5
lukkascost <lucas.costa@lit.ifce.edu.br>
2018-04-29 16:04:46 -0300

    module corba, replication implemented

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -1,56 +1,49 @@
 
 import Agenda.*;
+import org.omg.CORBA.ORB;
 import org.omg.CORBA.ORBPackage.InvalidName;
-import org.omg.CosNaming.*;
-import org.omg.CORBA.*;
-import org.omg.CosNaming.NamingContextPackage.CannotProceed;
-import org.omg.CosNaming.NamingContextPackage.NotFound;
-import org.omg.PortableServer.*;
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContext;
+import org.omg.CosNaming.NamingContextHelper;
 
 
 public class Cliente {
+    static String connectedIn = "No_Nodes";
+    static ContactsNode contactsNode = null;
+    static final String[] nodeNames = new String[]{"Agenda1","Agenda2", "Agenda3"};
+
 
     public static void main(String args[]) {
-        try {
-            ContactsNode contactsNode1;
-            ContactsNode contactsNode2;
-            ContactsNode contactsNode3;
-            ORB orb = ORB.init(args, null);
-            org.omg.CORBA.Object obj = null;
-
-            obj = orb.resolve_initial_references("NameService");
-
-            NamingContext naming = NamingContextHelper.narrow(obj);
-                NameComponent[] name1 = {new NameComponent("Agenda1", "Exemplo")};
-                NameComponent[] name2 = {new NameComponent("Agenda2", "Exemplo")};
-                NameComponent[] name3 = {new NameComponent("Agenda3", "Exemplo")};
-
-                org.omg.CORBA.Object objRef1 = naming.resolve(name1);
-                org.omg.CORBA.Object objRef2 = naming.resolve(name2);
-                org.omg.CORBA.Object objRef3 = naming.resolve(name3);
-
-                contactsNode1 = ContactsNodeHelper.narrow(objRef1);
-                contactsNode2 = ContactsNodeHelper.narrow(objRef2);
-                contactsNode3 = ContactsNodeHelper.narrow(objRef3);
-
-                contactsNode1.addContact(new Contact("lucas1","085997667126"));
-                contactsNode1.addContact(new Contact("lucas2","085997667126"));
-                contactsNode1.addContact(new Contact("lucas3","085997667126"));
-
-                System.out.println("ok");
-
-        } catch (InvalidName invalidName) {
-            invalidName.printStackTrace();
-        } catch (CannotProceed cannotProceed) {
-            cannotProceed.printStackTrace();
-        } catch (org.omg.CosNaming.NamingContextPackage.InvalidName invalidName) {
-            invalidName.printStackTrace();
-        } catch (Agenda.user_exists user_exists) {
-            user_exists.printStackTrace();
-        } catch (NotFound notFound) {
-            notFound.printStackTrace();
+        selectNode();
+        Contact contato1 = new Contact("lucas","997667126");
+        for(Contact c:contactsNode.data()){
+            System.out.printf(c.name+" - "+c.number);
         }
-
+        try {
+            contactsNode.addContact(contato1);
+        } catch (Agenda.user_exists user_exists) {
+            System.out.println("Contato já existe: "+ user_exists.contact.name+ " - "+user_exists.contact.number);
+        }
+        System.out.printf(connectedIn);
+    }
+    public static void selectNode(){
+        for(String node : nodeNames) {
+            try {
+                ORB orb = ORB.init(new String[0], null);
+                org.omg.CORBA.Object obj = null;
+                obj = orb.resolve_initial_references("NameService");
+                NamingContext naming = NamingContextHelper.narrow(obj);
+                NameComponent[] name = {new NameComponent(node, "Exemplo")};
+                org.omg.CORBA.Object objRef = null;
+                objRef = naming.resolve(name);
+                contactsNode = ContactsNodeHelper.narrow(objRef);
+                contactsNode.data();
+                connectedIn = node;
+                break;
+            } catch (Exception notFound) {
+                connectedIn = "No_Nodes";
+            }
+        }
     }
 
 }
