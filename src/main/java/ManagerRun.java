793eeea3cd600e2afc92ec84c755959bd79f5261
lukkascost <lucas.costa@lit.ifce.edu.br>
2018-05-25 16:06:10 -0300

    manage enviroment created

--- src/main/java/ManagerRun.java
+++ src/main/java/ManagerRun.java
@@ -1,20 +1,23 @@
+import Tup.Device;
 import Tup.Environment;
+import Tup.User;
 import net.jini.core.entry.UnusableEntryException;
 import net.jini.core.lease.Lease;
 import net.jini.core.transaction.TransactionException;
 import net.jini.space.JavaSpace;
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.IOException;
 import java.rmi.RemoteException;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class ManagerRun {
     public static boolean running = true;
     static Scanner in = new Scanner(System.in);
     static final String line = new String(new char[48]).replace('\0', '-');
     static JavaSpace space;
+    static Hashtable<String ,Integer > enviroments = new Hashtable<String, Integer>();
+    static Hashtable<String ,String > users = new Hashtable<String, String>();
+
 
     public static void main(String[] args) {
         try {
@@ -26,6 +29,7 @@ public class ManagerRun {
                 System.exit(-1);
             }
             System.out.println("O servico JavaSpace foi encontrado.");
+
             while (running){
                 clear();
                 title("Agenda de Contatos");
@@ -51,6 +55,11 @@ public class ManagerRun {
                             System.out.print("Pressione enter para continuar.... ");
                             System.in.read();
                             break ;
+                        case 2 :
+                            deleteEnvironment();
+                            System.out.print("Pressione enter para continuar.... ");
+                            System.in.read();
+                            break ;
                     }
                 }catch (InputMismatchException ex){
                     System.err.println("Digite um numero inteiro!!");
@@ -62,59 +71,70 @@ public class ManagerRun {
         }
     }
 
-    public static void listEnvironment(){
-        title("Lista de Ambientes");
-        int envCount = 1;
+    private static void deleteEnvironment() throws TransactionException, UnusableEntryException, RemoteException, InterruptedException {
+        title("Remover Ambiente");
+        System.out.print("Digite o nome do ambiente: ");
+        String name = in.next();
         Environment env = new Environment();
-        env.name = "amb"+envCount;
-        Environment result;
-        try {
-            result = (Environment) space.readIfExists(env, null, Long.MAX_VALUE);
-            while (result != null) {
-                System.out.println(envCount + " - "+result.name);
-                envCount++;
-                env.name = "amb"+envCount;
-                result = (Environment) space.readIfExists(env, null, Long.MAX_VALUE);
-            }
-        } catch (UnusableEntryException e) {
-            e.printStackTrace();
-        } catch (TransactionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (RemoteException e) {
-            e.printStackTrace();
+        env.name = name;
+        env = (Environment) space.readIfExists(env, null, Long.MAX_VALUE);
+        if(env == null) {
+            System.out.println(" Ambiente nao existe.");
+            return;
         }
+        User user = new User();
+        user.environment = name;
+        user = (User) space.readIfExists(user, null, Long.MAX_VALUE);
+        if(user != null ){
+            System.out.println(" Ambiente possui usuarios, mova-os antes de excluir.");
+            return;
+        }
+
+        Device dev = new Device();
+        dev.environment = name;
+        dev = (Device) space.readIfExists(dev,null,Long.MAX_VALUE);
+        if(dev != null){
+            System.out.println(" Ambiente possui dispositivos, mova-os antes de excluir.");
+            return;
+        }
+        env = new Environment();
+        env.name = name;
+        env = (Environment) space.take(env,null,Lease.FOREVER);
+
+
     }
 
-    public static void createEnvironment(){
+    public static void listEnvironment() throws RemoteException, TransactionException, InterruptedException, UnusableEntryException {
+        title("Lista de Ambientes");
+        getExistingEnvironments();
+        int i = 1;
+        for (String st:enviroments.keySet()) {
+            System.out.println(i+"| "+st);
+            i++;
+
+        }
+
+    }
+
+    public static void createEnvironment() throws TransactionException, UnusableEntryException, RemoteException, InterruptedException {
         title("Criar Ambientes");
         int envCount = 1;
         Environment env = new Environment();
         env.name = "amb"+envCount;
         Environment result;
-        try {
+        result = (Environment) space.readIfExists(env, null, Long.MAX_VALUE);
+        while (result != null) {
+            envCount++;
+            env.name = "amb"+envCount;
             result = (Environment) space.readIfExists(env, null, Long.MAX_VALUE);
-            while (result != null) {
-                envCount++;
-                env.name = "amb"+envCount;
-                result = (Environment) space.readIfExists(env, null, Long.MAX_VALUE);
-            }
-            result = new Environment();
-            result.name = "amb"+envCount;
-            result.chat = "";
-            space.write(result,null,Lease.FOREVER);
-            System.out.println("\t Ambiente Criado com sucesso ");
-
-        } catch (UnusableEntryException e) {
-            e.printStackTrace();
-        } catch (TransactionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (RemoteException e) {
-            e.printStackTrace();
         }
+        result = new Environment();
+        result.name = "amb"+envCount;
+        result.chat = "";
+        space.write(result,null,Lease.FOREVER);
+        System.out.println("\t Ambiente Criado com sucesso ");
+
+
     }
 
     public static void clear(){
@@ -126,6 +146,44 @@ public class ManagerRun {
         System.out.println(line);
         System.out.println(StringUtils.center(titleString, 50));
         System.out.println(line);
+    }
+
+    public static void getExistingEnvironments() throws TransactionException, UnusableEntryException, RemoteException, InterruptedException {
+        List<Environment> environmentList = new ArrayList<>();
+        enviroments = new Hashtable<>();
+        Environment result;
+        do{
+            result = new Environment();
+            result = (Environment) space.takeIfExists(result, null, Long.MAX_VALUE);
+            if (result != null){
+                environmentList.add(result);
+                enviroments.put(result.name,0);
+            }
+        }while (result!= null);
+
+        for (Environment e:environmentList) {
+            space.write(e,null, Lease.FOREVER);
+        }
+
+
+    }
+    public static void getExistingUsers() throws TransactionException, UnusableEntryException, RemoteException, InterruptedException {
+        List<User> userList = new ArrayList<>();
+        users = new Hashtable<>();
+        User result;
+        do{
+            result = new User();
+            result = (User) space.takeIfExists(result, null, Long.MAX_VALUE);
+            if (result != null){
+                userList.add(result);
+                users.put(result.name,result.environment);
+            }
+        }while (result!= null);
+
+        for (User u :userList) {
+            space.write( u ,null, Lease.FOREVER);
+        }
+
     }
 
 }
