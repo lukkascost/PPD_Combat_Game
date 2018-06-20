db6bf2dfce03dd9f98e2acb93e1747b33861ddc3
lukkascost <lucas.costa@lit.ifce.edu.br>
2018-06-20 01:33:59 -0300

    clean code and webService with OpenJMS

--- src/main/java/MainPackage/Publicador.java
+++ src/main/java/MainPackage/Publicador.java
@@ -4,6 +4,6 @@ import javax.xml.ws.Endpoint;
 
 public class Publicador {
     public static void main(String[] args) {
-        Endpoint.publish("http://localhost:9999/inverter", new InverterItfImpl());
+       // Endpoint.publish("http://localhost:9999/inverter", new InverterItfImpl());
     }
 }
